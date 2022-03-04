9e865e10d4aafca548f0d08d804317e8e7c2468f
Sahaj-makwana <92150038+Sahaj-makwana@users.noreply.github.com>
2022-03-04 11:00:33 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,3 +4,35 @@
 # 1. int
 x = 10
 print("x is of type ",type(x))
+
+#  float
+a = 7.2
+print("a is of type",type(a))
+
+#  complex
+a = 7+5j
+print("a is of type",type(a))
+
+# SEQUENCE TYPES
+#  list
+a = [1,2,3,4,5]
+print("a is of type",type(a))
+
+#  tuple
+a = (1,2,3,4,5)
+print("a is of type",type(a))
+
+# MAPPING TYPES
+# sets
+a = {1,2,3,4,5}
+print("a is of type",type(a))
+
+# TEXT TYPES
+# string
+a = 'Sahaj Makwana'
+print("a is of type",type(a))
+
+# BOOLEAN TYPES
+a = bool(4)
+print(a)
+print("a is of type",type(a))
